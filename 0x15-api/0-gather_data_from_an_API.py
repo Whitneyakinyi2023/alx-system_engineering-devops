0c6c4f1ff1fc18ffa32ac50078ba5ef3bc70b872
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-20 15:58:46 +0300

    working API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,44 +1,62 @@
 #!/usr/bin/python3
-"""Writing  a python script using REST API"""
 
+"""
+Python script that, using a REST API
+"""
 
 from requests import get
 from sys import argv
 
 
+def main():
+  if len(argv) != 2:
+    print("Usage: python script.py <employee_id>")
+    exit(1)
 
-def to_do_list_info(employee_id):
-    """base url fr accessing API"""
-    
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    
-    user_response = get(f'{base_url}/users{employee_id}')
-    if user_response.status_code != 200:
-        print(f'Error: Employee with ID {employee_id} not found.')
-        user = user_response.json()
-        employee_name = user.get('name')
-        todos_response = get(f'{base_url}/todos?userId={employee_id}')
-        """Fetching the to-do list"""
-        todos = todos_response.json()
-        
-        total_tasks = len(todos)
-        """Calculation of total number  of tasks"""
-        done_tasks = [todo for todo in todos if todo.get('completed')]
-        number_of_done_tasks = len(done_tasks)
-        
-        print(f'Employee {employee_name} is done with tasks'
-              f'({number_of_done_tasks}/{total_tasks}): ')
-        """Printing out the output:"""
-        
-        for task in done_tasks:
-            print(f'\t {task.get("title")}')
-            
-        if __name__ == "__main__":
-            if len(sys.argv) != 2:
-                print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        else:
-             try:
-                employee_id = int(sys.argv[1])
-                to_do_list_info(employee_id)
-             except ValueError:
-                 print("Employee ID must be an integer.")
+  try:
+    employee_id = int(argv[1])
+  except ValueError:
+    print("Invalid employee ID: Please provide an integer.")
+    exit(1)
+
+  todos_url = 'https://jsonplaceholder.typicode.com/todos/'
+  users_url = 'https://jsonplaceholder.typicode.com/users'
+
+  todos_response = get(todos_url)
+  if todos_response.status_code == 200:
+      todos_data = todos_response.json()
+  else:
+      print(f"Error retrieving TODO data: {todos_response.status_code}")
+      exit(1)
+
+  user_response = get(users_url)
+  if user_response.status_code == 200:
+      user_data = user_response.json()
+  else:
+      print(f"Error retrieving user data: {user_response.status_code}")
+      exit(1)
+
+  completed_tasks = 0
+  total_tasks = 0
+  completed_task_titles = []
+
+  for user in user_data:
+    if user.get('id') == employee_id:
+      employee_name = user.get('name')
+
+  for todo in todos_data:
+    if todo.get('userId') == employee_id:
+      total_tasks += 1
+
+      if todo.get('completed') is True:
+        completed_tasks += 1
+        completed_task_titles.append(todo.get('title'))
+
+  print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
+
+  for task_title in completed_task_titles:
+    print("\t {}".format(task_title))
+
+
+if __name__ == "__main__":
+  main()
