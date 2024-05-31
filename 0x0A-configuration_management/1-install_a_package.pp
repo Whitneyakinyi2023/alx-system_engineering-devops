d5c05cd5ef52ac90585ff91568b6140002c7a98e
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-31 15:40:15 +0300

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,18 @@
 # Automated installation of  puppet-lint -v 2.5.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Define a class to manage Flask installation
+class { 'flask':
+  # Ensure Flask is installed with pip3
+  package { 'Flask':
+    ensure => present,
+    provider => 'pip3',
+    require => undef,  # Remove any unnecessary dependencies
+  }
+  
+  # Specify the exact version (2.1.0)
+  $flask_version = '2.1.0'
+  # Install Flask with the specific version
+  python::pip { 'Flask':
+    ensure => $flask_version,
+  }
 }
