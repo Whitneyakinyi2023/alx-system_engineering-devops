626a91d8781a2922396a81aa42f46976bc2d5e52
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-31 16:07:13 +0300

    completed advanced tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,11 @@
-# Automated installation of  puppet-lint -v 2.5.0
-package {'flask':
-    provider => 'pip3',
-    ensure   => '2.1.0',
-    }
-
-exec { 'puppet-lint':
-command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+#Automated installatio n of flask and puppet
+exec { 'install_puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint=2.5.0',
+  unless  => 'dpkg -s puppet-lint | grep -q "Version: 2.5.0"',
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/apt-get -y install python3-flask=2.1.0',
+  unless  => 'dpkg -s python3-flask | grep -q "Version: 2.1.0"',
+}
+
