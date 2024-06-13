d54d9a56e0c79c06b95945796d06647ed0f5cc94
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-06-13 10:19:58 +0000

    puppet lint validation

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 
 exec {'modify max open files limit setting':
   command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
-  path => 'usr/local/sbin:/usr/local/bin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+  path    => 'usr/local/sbin:/usr/local/bin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
